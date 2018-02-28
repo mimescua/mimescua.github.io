014453abbfb044cd3bfcc2e6788af9f55b24aec4
mimescua <mescua@outlook.com>
2018-02-28 09:37:35 -0500

    Add files via upload

---
+++ AstrumUC/js/site.js
@@ -0,0 +1,8 @@
+﻿// Write your Javascript code.
+//fix z-index for hover effect
+$(function () {
+    $(".work-item").on("mouseover", function () {
+        $(".work-item").css("z-index", "auto");
+        $(this).css("z-index", "1000");
+    });
+});

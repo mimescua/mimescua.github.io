f4c7d2a4c62a434ae001ba0ae56da4ec9a679238
mimescua <mescua@outlook.com>
2019-02-27 12:35:48 -0500

    Delete site.js

--- AstrumUC/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿// Write your Javascript code.
-//fix z-index for hover effect
-$(function () {
-    $(".work-item").on("mouseover", function () {
-        $(".work-item").css("z-index", "auto");
-        $(this).css("z-index", "1000");
-    });
-});
